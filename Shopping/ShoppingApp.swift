d67de091c82a93edbbd01f3a12b771a2e6b013ca
CodingBot000 <pokerface582@gmail.com>
2024-08-22 14:43:40 +0900

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by switchMac on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
